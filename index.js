52057b47a840fb06e0434db9eea5bc86bd206926
SkyLined <berendjanwever@gmail.com>
2015-04-28 10:49:26 +0200

    Fix index.js "(" bug

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var path = require("path");
 var sModuleName = path.basename(__dirname);
 module.exports.fConnect = require("./fConnect");
 module.exports.cServer = require("./cServer");
-module.exports.mSettings = require(("./mSettings");
+module.exports.mSettings = require("./mSettings");
